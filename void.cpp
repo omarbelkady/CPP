99d3b15cc9644129d4c23333dd292d671d2806bc
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-15 20:57:33 +0100

    Create void.cpp
    The void keyword in C++

---
+++ void.cpp
@@ -0,0 +1,15 @@
+//Void Functions
+#include<iostream>
+using namespace std;
+
+//creating a functions outside of main method
+void printSmthn()
+{
+    cout<<"this is a void function that doesn't return anything"<<endl;
+}
+main()
+{
+    printSmthn();
+    return 0;
+}
+
