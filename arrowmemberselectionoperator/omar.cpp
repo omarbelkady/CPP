7d80a0c117f13170c223148ec2ff3b9fa3f58b0d
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-04-15 21:35:51 +0100

    Create omar.cpp

---
+++ arrowmemberselectionoperator/omar.cpp
@@ -0,0 +1,13 @@
+#include "Omar.h"
+#include<iostream>
+using namespace std;
+
+Omar::Omar()
+{
+    //this is my constructor
+}
+
+void Omar::printClouds()
+{
+    cout<<"Clouds are wishy washy and fluffy"<<endl;
+}
