d9ecbddae132ac1a05fd5953d86602b336bf0f2c
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-06-26 16:06:05 +0100

    Update and rename prntmultidimenarray.cpp to PrintAMultiDimArray.cpp

---
+++ PrintAMultiDimArray.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+using namespace std;
+main()
+{
+	int millennial[2][3]={{6,7,8},{18,24,27}};
+	for(int row=0;row<2;row++)
+	{
+		for(int column=0;column<3;column++)
+		{
+			cout<<millennial[row][column]<<" ";
+		}
+	    cout<<endl;
+	}
+}
