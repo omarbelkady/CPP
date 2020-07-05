7a47ddf711d82f18f81f2427f6792d9738d58971
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-05 11:16:18 +0100

    Create Getline.cpp

---
+++ Getline.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+main(){
+    string helloThere;
+    //Used to get a line of string
+    getline(cin,helloThere);
+    cout<<helloThere;
+    
+    //cin.getline is used to get a line of ints, doubles
+}
