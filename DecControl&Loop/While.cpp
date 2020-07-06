5523b36b6178caad0dc165ba38ab3063498c8f99
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-06 18:18:51 +0100

    Create While.cpp

---
+++ DecControl&Loop/While.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+void printHi(){
+    cout<<"Hi"<<endl;
+}
+
+main()
+{
+    int x=1;
+    
+    while(x != 4)
+    {
+        printHi();
+        x++;
+    }
+    
+    cout<<"I am being annoying you 6342 56837";
+}
