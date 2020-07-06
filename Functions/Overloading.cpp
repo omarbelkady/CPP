152fa9a997eafe3ed2cf332274a4f41f8b545014
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-06 18:11:50 +0100

    Update and rename Overloading.cpp to Functions/Overloading.cpp

---
+++ Functions/Overloading.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+void printNumber(int x)
+{
+	 cout<<x<<endl;
+}
+//To overload keep the same name and have different number of parameters or different input type 
+void printNumber(float x)
+{
+	 cout<<x<<endl;
+}
+
+
+int main()
+{
+	int a=98;
+	float b=37.05919;
+	
+	
+	
+	printNumber(a);
+	printNumber(b);
+	
+}
