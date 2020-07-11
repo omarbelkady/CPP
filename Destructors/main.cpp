623efc0ae0d51c655c6e94bf0949958d6bbf7570
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-12 00:03:23 +0100

    Create main.cpp

---
+++ Destructors/main.cpp
@@ -0,0 +1,10 @@
+//Arrow Member Selection Operator -->
+#include "Omar.h"
+#include<iostream>
+
+using namespace std;
+main()
+{
+    Omar oo;
+    cout<<"What is this printing on my screen"<<endl;
+}
