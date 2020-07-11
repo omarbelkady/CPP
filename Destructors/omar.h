fe9cf36ffb39f506d20cca66b99796b9977294c8
omarbelkady <31806568+omarbelkady@users.noreply.github.com>
2020-07-12 00:09:29 +0100

    Create omar.h

---
+++ Destructors/omar.h
@@ -0,0 +1,21 @@
+#ifndef OMAR_H
+#define OMAR_H
+
+
+class Omar
+{
+    public: 
+        Omar();
+        //A destructor is a member function that is called automatically when the object
+        //is out of scope(meaning we have lost the privellege because of the execution/control flow of the program)
+        //A second way to destructor is a call to explicitly delete it. We call a destructor by placing a tilde in front
+        //of the class name.
+        ~Omar();
+    private:
+        
+    protected:
+  
+};
+
+
+#endif
