d323060ce335abeb325e3926b6df0bd61e213631
Omar <omar.belkady@g.austincc.edu>
2020-08-01 00:19:30 +0100

    Alan said Structs are good to work with in C and easy and C is always the best for him

--- Pointers/Arrow_Struct.cpp
+++ Pointers/Arrow_Struct.cpp
@@ -1,17 +1,21 @@
 #include<iostream>
 #include<string>
 
+using namespace std;
+
 struct my_Vector{
 	float a,b,c;
+	float Nelan_And_C;
 };
 
 int main(){
 	int my_out = (int)&((my_Vector*)nullptr)->c;//prints 8 for 8 bytes
 	int my_four = (int)&((my_Vector*)nullptr)->b;//prints 4 for 4 bytes
-	std::cout<<my_out<<std::endl;
+	cout<<my_out<<endl;
 	int my_two = (int)&((my_Vector*)nullptr)->a;//prints 2 for 2 bytes
-	std::cout<<"The output for the first element is: "<<my_two<<" because we are allocating 0  bytes so damn cool!!"<<std::endl;
-	std::cout<<"The output is now for the 2nd element is: "<<my_four<<std::endl;
-	std::cout<<"The output for the the 3rd element is: "<<my_out<<std::endl;
-	std::cin.get();
+	float my_fst_float=(int)&((my_Vector*)nullptr)->Nelan_And_C;
+	cout<<"The output for the first element is: "<<my_two<<" because we are allocating 0  bytes so damn cool!!"<<endl;
+	cout<<"The output is now for the 2nd element is: "<<my_four<<endl;
+	cout<<"The output for the the 3rd element is: "<<my_out<<endl;
+	cout<<"Alan Loves C Value: "<<my_fst_float<<endl;
 }
